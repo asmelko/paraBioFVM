06624c172c88906acb7a3b58d654942f66f55f8e
Adam Smelko <adamsmelko1@gmail.com>
2023-05-18 12:15:58 +0200

    better diffusion test

--- test/test.cpp
+++ test/test.cpp
@@ -27,6 +27,26 @@ microenvironment default_microenv(cartesian_mesh mesh)
 							std::move(initial_conds));
 }
 
+microenvironment biorobots_microenv(cartesian_mesh mesh)
+{
+	real_t diffusion_time_step = 0.01;
+	index_t substrates_count = 2;
+
+	auto diff_coefs = std::make_unique<real_t[]>(2);
+	diff_coefs[0] = 1000;
+	diff_coefs[1] = 1000;
+	auto decay_rates = std::make_unique<real_t[]>(2);
+	decay_rates[0] = 0.1;
+	decay_rates[1] = 0.4;
+
+	auto initial_conds = std::make_unique<real_t[]>(2);
+	initial_conds[0] = 0;
+	initial_conds[1] = 0;
+
+	return microenvironment(mesh, substrates_count, diffusion_time_step, std::move(diff_coefs), std::move(decay_rates),
+							std::move(initial_conds));
+}
+
 TEST(host_diffusion_solver, D1)
 {
 	cartesian_mesh mesh(1, { 0, 0, 0 }, { 80, 0, 0 }, { 20, 0, 0 });
@@ -77,7 +97,7 @@ TEST(host_diffusion_solver, D2)
 	});
 }
 
-TEST(host_diffusion_solver, D3)
+TEST(host_diffusion_solver, D3_uniform)
 {
 	cartesian_mesh mesh(3, { 0, 0, 0 }, { 200, 200, 200 }, { 20, 20, 20 });
 
@@ -100,6 +120,133 @@ TEST(host_diffusion_solver, D3)
 		EXPECT_FLOAT_EQ(l | noarr::get_at<'s'>(m.substrate_densities.get(), 0), 0.0012299563);
 		EXPECT_FLOAT_EQ(l | noarr::get_at<'s'>(m.substrate_densities.get(), 1), 0.0046296306);
 	});
+}
+
+TEST(host_diffusion_solver, D1_random)
+{
+	cartesian_mesh mesh(1, { 0, 0, 0 }, { 60, 0, 0 }, { 20, 20, 20 });
+
+	index_t substrates_count = 2;
+	auto m = biorobots_microenv(mesh);
+
+	auto dens_l = layout_traits<1>::construct_density_layout(substrates_count, mesh.grid_shape);
+
+	// fill with random values
+	for (index_t s = 0; s < m.substrates_count; ++s)
+		for (index_t x = 0; x < mesh.grid_shape[0]; ++x)
+		{
+			index_t index = s + x * m.substrates_count;
+			(dens_l | noarr::get_at<'x', 's'>(m.substrate_densities.get(), x, s)) = index;
+		}
+
+	diffusion_solver s;
+
+	s.initialize(m);
+
+	s.solve(m);
+
+	std::vector<float> expected = {
+		0.0486842592, 1.0444132121, 1.9980019980, 2.9880478088, 3.9473197368, 4.9316824055
+	};
+
+	for (index_t s = 0; s < m.substrates_count; ++s)
+		for (index_t x = 0; x < mesh.grid_shape[0]; ++x)
+		{
+			index_t index = s + x * m.substrates_count;
+
+			EXPECT_FLOAT_EQ((dens_l | noarr::get_at<'x', 's'>(m.substrate_densities.get(), x, s)), expected[index]);
+		}
+}
+
+TEST(host_diffusion_solver, D2_random)
+{
+	cartesian_mesh mesh(2, { 0, 0, 0 }, { 60, 60, 0 }, { 20, 20, 20 });
+
+	index_t substrates_count = 2;
+	auto m = biorobots_microenv(mesh);
+
+	auto dens_l = layout_traits<2>::construct_density_layout(substrates_count, mesh.grid_shape);
+
+	// fill with random values
+	for (index_t s = 0; s < m.substrates_count; ++s)
+		for (index_t x = 0; x < mesh.grid_shape[0]; ++x)
+			for (index_t y = 0; y < mesh.grid_shape[1]; ++y)
+			{
+				index_t index = s + x * m.substrates_count + y * m.substrates_count * mesh.grid_shape[0];
+				(dens_l | noarr::get_at<'x', 'y', 's'>(m.substrate_densities.get(), x, y, s)) = index;
+			}
+
+	diffusion_solver s;
+
+	s.initialize(m);
+
+	s.solve(m);
+
+	std::vector<float> expected = { 0.1948319355,  1.1899772978,  2.1441254507,	 3.1335099015,	4.0934189658,
+									5.0770425053,  6.0427124809,  7.0205751090,	 7.9920082,		8.9641077127,
+									9.9412995111,  10.9076403164, 11.8905930262, 12.8511729202, 13.8398865413,
+									14.7947055239, 15.7891800565, 16.7382381276 };
+
+	for (index_t s = 0; s < m.substrates_count; ++s)
+		for (index_t x = 0; x < mesh.grid_shape[0]; ++x)
+			for (index_t y = 0; y < mesh.grid_shape[1]; ++y)
+			{
+				index_t index = s + x * m.substrates_count + y * m.substrates_count * mesh.grid_shape[0];
+
+				EXPECT_FLOAT_EQ((dens_l | noarr::get_at<'x', 'y', 's'>(m.substrate_densities.get(), x, y, s)),
+								expected[index]);
+			}
+}
+
+TEST(host_diffusion_solver, D3_random)
+{
+	cartesian_mesh mesh(3, { 0, 0, 0 }, { 60, 60, 60 }, { 20, 20, 20 });
+
+	index_t substrates_count = 2;
+	auto m = biorobots_microenv(mesh);
+
+	auto dens_l = layout_traits<3>::construct_density_layout(substrates_count, mesh.grid_shape);
+
+	// fill with random values
+	for (index_t s = 0; s < m.substrates_count; ++s)
+		for (index_t x = 0; x < mesh.grid_shape[0]; ++x)
+			for (index_t y = 0; y < mesh.grid_shape[1]; ++y)
+				for (index_t z = 0; z < mesh.grid_shape[2]; ++z)
+				{
+					index_t index = s + x * m.substrates_count + y * m.substrates_count * mesh.grid_shape[0]
+									+ z * m.substrates_count * mesh.grid_shape[0] * mesh.grid_shape[1];
+					(dens_l | noarr::get_at<'x', 'y', 'z', 's'>(m.substrate_densities.get(), x, y, z, s)) = index;
+				}
+
+	diffusion_solver s;
+
+	s.initialize(m);
+
+	s.solve(m);
+
+	std::vector<float> expected = {
+		0.6333066643,  1.6268066007,  2.5825920996,	 3.5703051208,	4.5318775349,  5.5138036408,  6.4811629703,
+		7.4573021609,  8.4304484056,  9.4008006809,	 10.3797338410, 11.3442992010, 12.3290192763, 13.2877977210,
+		14.2783047117, 15.2312962410, 16.2275901470, 17.1747947611, 18.1768755823, 19.1182932811, 20.1261610177,
+		21.0617918012, 22.0754464530, 23.0052903212, 24.0247318884, 24.9487888412, 25.9740173237, 26.8922873613,
+		27.9233027591, 28.8357858813, 29.8725881944, 30.7792844014, 31.8218736297, 32.7227829214, 33.7711590651,
+		34.6662814414, 35.7204445004, 36.6097799615, 37.6697299358, 38.5532784815, 39.6190153711, 40.4967770016,
+		41.5683008064, 42.4402755216, 43.5175862418, 44.3837740416, 45.4668716771, 46.3272725617, 47.4161571125,
+		48.2707710817, 49.3654425478, 50.2142696018, 51.3147279832, 52.1577681218
+	};
+
+	for (index_t s = 0; s < m.substrates_count; ++s)
+		for (index_t x = 0; x < mesh.grid_shape[0]; ++x)
+			for (index_t y = 0; y < mesh.grid_shape[1]; ++y)
+				for (index_t z = 0; z < mesh.grid_shape[2]; ++z)
+				{
+					index_t index = s + x * m.substrates_count + y * m.substrates_count * mesh.grid_shape[0]
+									+ z * m.substrates_count * mesh.grid_shape[0] * mesh.grid_shape[1];
+
+					EXPECT_FLOAT_EQ(
+						(dens_l | noarr::get_at<'x', 'y', 'z', 's'>(m.substrate_densities.get(), x, y, z, s)),
+						expected[index]);
+				}
 }
 
 void add_dirichlet_at(microenvironment& m, index_t substrates_count, const std::vector<point_t<index_t, 3>>& indices,
