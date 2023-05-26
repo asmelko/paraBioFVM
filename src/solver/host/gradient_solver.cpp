81ccee53f03130bd6ab5bc86340d5a46e8e3beb8
Adam Smelko <adamsmelko1@gmail.com>
2023-05-26 11:28:30 +0200

    gradient teams

--- src/solver/host/gradient_solver.cpp
+++ src/solver/host/gradient_solver.cpp
@@ -100,6 +100,7 @@ void solve_z(real_t* __restrict__ gradients, real_t* __restrict__ densities, ind
 	auto grad_l2 = grad_l ^ noarr::merge_blocks<'x', 's', 'X'>();
 	auto dens_l2 = dens_l ^ noarr::merge_blocks<'x', 's', 'X'>();
 
+	#pragma omp for
 	for (index_t y = 0; y < y_dim; y++)
 	{
 		for (index_t x = 0; x < x_dim * s_dim; x++)
@@ -111,6 +112,7 @@ void solve_z(real_t* __restrict__ gradients, real_t* __restrict__ densities, ind
 		}
 	}
 
+	#pragma omp for
 	for (index_t z = 1; z < z_dim - 1; z++)
 	{
 		for (index_t y = 0; y < y_dim; y++)
@@ -125,6 +127,7 @@ void solve_z(real_t* __restrict__ gradients, real_t* __restrict__ densities, ind
 		}
 	}
 
+	#pragma omp for
 	for (index_t y = 0; y < y_dim; y++)
 	{
 		for (index_t x = 0; x < x_dim * s_dim; x++)
@@ -144,39 +147,37 @@ void solve_3d_internal(real_t* __restrict__ gradients, real_t* __restrict__ dens
 	const index_t z_dim = dens_l | noarr::get_length<'z'>();
 	const index_t y_dim = dens_l | noarr::get_length<'y'>();
 
-	omp_set_num_threads(3);
-
-#pragma omp parallel
+#pragma omp teams num_teams(3)
 	{
-#pragma omp sections
+		if (omp_get_team_num() == 0)
 		{
-#pragma omp section
+#pragma omp for
+			for (index_t z = 0; z < z_dim; z++)
 			{
-				for (index_t z = 0; z < z_dim; z++)
+				for (index_t y = 0; y < y_dim; y++)
 				{
-					for (index_t y = 0; y < y_dim; y++)
-					{
-						solve_x(gradients, densities, voxel_shape[0], dens_l ^ noarr::fix<'y', 'z'>(y, z),
-								grad_l ^ noarr::fix<'y', 'z', 'd'>(y, z, noarr::lit<0>));
-					}
+					solve_x(gradients, densities, voxel_shape[0], dens_l ^ noarr::fix<'y', 'z'>(y, z),
+							grad_l ^ noarr::fix<'y', 'z', 'd'>(y, z, noarr::lit<0>));
 				}
-			}
-
-#pragma omp section
-			{
-				for (index_t z = 0; z < z_dim; z++)
-				{
-					solve_y(gradients, densities, voxel_shape[1], dens_l ^ noarr::fix<'z'>(z),
-							grad_l ^ noarr::fix<'z', 'd'>(z, noarr::lit<1>));
-				}
-			}
-
-#pragma omp section
-			{
-				solve_z(gradients, densities, voxel_shape[2], dens_l, grad_l ^ noarr::fix<'d'>(noarr::lit<2>));
 			}
 		}
+
+		if (omp_get_team_num() == 1)
+		{
+#pragma omp for
+			for (index_t z = 0; z < z_dim; z++)
+			{
+				solve_y(gradients, densities, voxel_shape[1], dens_l ^ noarr::fix<'z'>(z),
+						grad_l ^ noarr::fix<'z', 'd'>(z, noarr::lit<1>));
+			}
+		}
+
+		if (omp_get_team_num() == 2)
+		{
+			solve_z(gradients, densities, voxel_shape[2], dens_l, grad_l ^ noarr::fix<'d'>(noarr::lit<2>));
+		}
 	}
+}
 }
 
 template <typename density_layout_t, typename gradient_layout_t>
