3adc6454b79fa1a343cf713d200574eca8fbcaca
Adam Smelko <adamsmelko1@gmail.com>
2023-06-09 14:46:30 +0200

    fix resizing of agent_data

--- src/agent_data.cpp
+++ src/agent_data.cpp
@@ -8,23 +8,19 @@ agent_data::agent_data(microenvironment& m) : agents_count(0), m(m) {}
 
 void agent_data::add()
 {
-    agents_count++;
+	agents_count++;
 
-    secretion_rates.resize(agents_count * m.substrates_count);
-    saturation_densities.resize(agents_count * m.substrates_count);
-    uptake_rates.resize(agents_count * m.substrates_count);
-    net_export_rates.resize(agents_count * m.substrates_count);
+	secretion_rates.resize(agents_count * m.substrates_count);
+	saturation_densities.resize(agents_count * m.substrates_count);
+	uptake_rates.resize(agents_count * m.substrates_count);
+	net_export_rates.resize(agents_count * m.substrates_count);
 
-    internalized_substrates.resize(agents_count * m.substrates_count, 0);
-    fraction_released_at_death.resize(agents_count * m.substrates_count);
-    fraction_transferred_when_ingested.resize(agents_count * m.substrates_count);
+	internalized_substrates.resize(agents_count * m.substrates_count, 0);
+	fraction_released_at_death.resize(agents_count * m.substrates_count);
+	fraction_transferred_when_ingested.resize(agents_count * m.substrates_count);
 
-    volumes.push_back(0);
-
-    for (index_t i = 0; i < m.mesh.dims; i++)
-    {
-        positions.push_back(0);
-    }
+	volumes.resize(agents_count, 0);
+	positions.resize(agents_count * m.mesh.dims, 0);
 }
 
 void agent_data::remove(index_t index)
