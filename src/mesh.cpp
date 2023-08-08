057ce01c5b45c4f964f269da2f856802533ba0c7
Adam Smelko <adamsmelko1@gmail.com>
2023-08-08 09:57:42 +0200

    fix grid shape computation

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -12,15 +12,15 @@ cartesian_mesh::cartesian_mesh(index_t dims, point_t<index_t, 3> bounding_box_mi
 
 	if (dims >= 1)
 	{
-		grid_shape[0] = (bounding_box_maxs[0] - bounding_box_mins[0]) / voxel_shape[0];
+		grid_shape[0] = (bounding_box_maxs[0] - bounding_box_mins[0] + voxel_shape[0] - 1) / voxel_shape[0];
 	}
 	if (dims >= 2)
 	{
-		grid_shape[1] = (bounding_box_maxs[1] - bounding_box_mins[1]) / voxel_shape[1];
+		grid_shape[1] = (bounding_box_maxs[1] - bounding_box_mins[1] + voxel_shape[1] - 1) / voxel_shape[1];
 	}
 	if (dims >= 3)
 	{
-		grid_shape[2] = (bounding_box_maxs[2] - bounding_box_mins[2]) / voxel_shape[2];
+		grid_shape[2] = (bounding_box_maxs[2] - bounding_box_mins[2] + voxel_shape[2] - 1) / voxel_shape[2];
 	}
 }
 
