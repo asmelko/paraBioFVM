ebfe552c987cb09080b4b0461b5dd9d6e87d6023
Adam Smelko <adamsmelko1@gmail.com>
2023-08-19 18:33:56 +0200

    double precision as cmake option openmp again optional

--- include/BioFVM/types.h
+++ include/BioFVM/types.h
@@ -6,8 +6,13 @@
 namespace biofvm {
 
 using agent_id_t = uint32_t;
-using real_t = float;
 using index_t = int32_t;
+
+#ifdef USE_DOUBLES
+using real_t = double;
+#else
+using real_t = float;
+#endif
 
 template <typename T, int dims>
 using point_t = std::array<T, dims>;
