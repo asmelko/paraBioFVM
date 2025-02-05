805fe48dc791696c6f771e49bd234e88f68b0974
Adam Smelko <adamsmelko1@gmail.com>
2025-02-05 15:57:36 +0100

    Call serialize in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "solver/host/solver.h"
+#include "vtk_serializer.h"
 
 using namespace biofvm;
 
@@ -42,16 +43,16 @@ int main()
 {
 	cartesian_mesh mesh(3, { 0, 0, 0 }, { 5000, 5000, 5000 }, { 20, 20, 20 });
 
-	real_t diffusion_time_step = 5;
+	real_t diffusion_time_step = 1;
 	index_t substrates_count = 4;
 
 	auto diff_coefs = std::make_unique<real_t[]>(substrates_count);
-	diff_coefs[0] = 4;
+	diff_coefs[0] = 10000;
 	auto decay_rates = std::make_unique<real_t[]>(substrates_count);
-	decay_rates[0] = 5;
+	decay_rates[0] = 0;
 
 	auto initial_conds = std::make_unique<real_t[]>(substrates_count);
-	initial_conds[0] = 0;
+	initial_conds[0] = 400;
 
 	microenvironment m(mesh, substrates_count, diffusion_time_step, initial_conds.get());
 
@@ -59,11 +60,21 @@ int main()
 	m.decay_rates = std::move(decay_rates);
 	m.compute_internalized_substrates = true;
 
+	m.dirichlet_min_boundary_conditions[0] = std::make_unique<bool[]>(substrates_count);
+	m.dirichlet_min_boundary_conditions[0][0] = true;
+	m.dirichlet_min_boundary_values[0] = std::make_unique<real_t[]>(substrates_count);
+	m.dirichlet_min_boundary_values[0][0] = 10000;
+
+	for (index_t i = 0; i < substrates_count; i++)
+		m.substrates_names.push_back(std::string("s") + std::to_string(i));
+
 	make_agents(m, 2'000'000, true);
 
 	biofvm::solvers::host::solver s;
 
 	s.initialize(m);
+
+	vtk_microenvironment_serializer serializer("output", m);
 
 	for (index_t i = 0; i < 100; ++i)
 	{
@@ -100,9 +111,25 @@ int main()
 				secretion_duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 			}
 
+#pragma omp barrier
 #pragma omp master
-			std::cout << "Diffusion time: " << diffusion_duration << " ms,\t Gradient time: " << gradient_duration
-					  << " ms,\t Secretion time: " << secretion_duration << " ms" << std::endl;
+			{
+				size_t serialize_duration;
+				{
+					auto start = std::chrono::high_resolution_clock::now();
+
+					serializer.serialize_one_timestep(m);
+
+					auto end = std::chrono::high_resolution_clock::now();
+
+					serialize_duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+				}
+
+				std::cout << "Diffusion time: " << diffusion_duration << " ms,\t Gradient time: " << gradient_duration
+						  << " ms,\t Secretion time: " << secretion_duration
+						  << " ms,\t Serialize time: " << serialize_duration << " ms" << std::endl;
+			}
+#pragma omp barrier
 		}
 	}
 
